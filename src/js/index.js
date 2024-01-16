64924110a3093ef8627436760c2fc4f4298ff24a
Žofie Tůmová <zofietumova@gmail.com>
2024-01-16 20:39:30 +0100

    final

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
-console.log("Test skriptu");
+
+let counter = 0;
+
+const counterValue = document.getElementById("counterValue");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const resetButton = document.getElementById("resetButton");
+
+plus.addEventListener("click", function plusOne() {
+    counter++;
+    counterValue.innerHTML = counter;
+});
+
+minus.addEventListener("click", function minusOne() {
+    counter--;
+    counterValue.innerHTML = counter;
+});
+
+resetButton.addEventListener("click", reset);
+
+function reset() {
+    counter = 0;
+    counterValue.innerHTML = counter;
+}
