18a6b7cc2e4b8b6c5d58da754e62474a0aa9fdcf
Žofie Tůmová <zofietumova@gmail.com>
2024-01-19 15:03:18 +0100

    zofka verze

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,30 @@
-
 const container = document.getElementById("container");
 let selectedText = "";
 let rangeOfText = "";
 let noteContent = JSON.parse(localStorage.getItem("noteContent") || "[]");
 
+function saveNoteData() {
+  console.log("saving");
+  noteContent = [];
+  localStorage.setItem("noteContent", []);
+  const noteEditor = document.querySelectorAll(".noteEditor");
+  noteEditor.forEach((stickyNote) => {
+    if (stickyNote.innerHTML !== "") {
+      const HTML = {
+        value: stickyNote.innerHTML
+      };
+      noteContent.push(HTML);
+    }
+  });
+
+  localStorage.setItem("noteContent", JSON.stringify(noteContent));
+}
+
 function createNewNote(initialContent) {
-    let stickyNote = document.createElement("div");
-    stickyNote.classList.add("noteRow");
-    let htmlForNote =
-        `<div contenteditable="true" class="noteEditor" id="noteEditor" onmouseup="getSelectedText()">` +
-        initialContent +
-        `</div>
+  const stickyNote = document.createElement("div");
+  stickyNote.classList.add("note");
+  const htmlForNote = `
+        <div contenteditable="true" class="noteEditor" id="noteEditor" onmouseup="getSelectedText()">${initialContent}</div>
         <div class="noteControls">
         <div onclick="getSelected('capitalize')" class="capitalize">Aa</div>
         <div onclick="getSelected('bold')" class="bold">B</div>
@@ -19,74 +33,57 @@ function createNewNote(initialContent) {
         <div onclick="getSelected('lineThrough')" class="lineThrough">ab</div>
         <hr />
         <img src="images/bin.png" id="bin" class="bin"onclick="DeleteNote(this)" />
-        </div>`;
-    stickyNote.innerHTML = htmlForNote;
-    container.appendChild(stickyNote);
+        </div>
+  `;
+  stickyNote.innerHTML = htmlForNote;
+  container.appendChild(stickyNote);
 
-    const noteEditor = document.querySelectorAll(".noteEditor");
-    noteEditor.forEach((el) =>
-        el.addEventListener("keypress", (event) => {
-            if (event.key === "Enter") {
-                //document.execCommand("insertHTML", false, "<br/>");
-                //return false;
-                el.innerHTML +- "<br>";
-                event.preventDefault();
-            }
-        })
-    );
-    SaveNoteData();
+  const noteEditor = document.querySelectorAll(".noteEditor");
+  noteEditor.forEach((el) => el.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      // document.execCommand("insertHTML", false, "<br/>");
+      // return false;
+      //el.innerHTML += "<br>";
+      event.preventDefault();
+    }
+  }));
+  saveNoteData();
 }
 
-document.addEventListener('keydown', stickyNote => {
-    if (stickyNote.ctrlKey && stickyNote.key === 's') {
-        stickyNote.preventDefault();
-        SaveNoteData();
-    }
+document.addEventListener("keydown", (stickyNote) => {
+  if (stickyNote.ctrlKey && stickyNote.key === "s") {
+    stickyNote.preventDefault();
+    saveNoteData();
+  }
 });
-
-function SaveNoteData() {
-    noteContent = [];
-    localStorage.setItem("noteContent", []);
-    const noteEditor = document.querySelectorAll(".noteEditor");
-    noteEditor.forEach((stickyNote) => {
-        if (stickyNote.innerHTML !== "") {
-            let HTML = {
-                value: stickyNote.innerHTML
-            };
-            noteContent.push(HTML);
-        }
-    });
-
-    localStorage.setItem("noteContent", JSON.stringify(noteContent));
-};
 
 readData();
 
 function readData() {
-    noteContent.forEach((element) => {
-        createNewNote(element.value + "<br />");
-    });
+  noteContent.forEach((element) => {
+    createNewNote(element.value + "<br />");
+  });
 }
 
 function getSelectedText() {
-    selectedText = window.getSelection().toString();
-    rangeOfText = window.getSelection().getRangeAt(0);
-};
+  selectedText = window.getSelection().toString();
+  rangeOfText = window.getSelection().getRangeAt(0);
+}
 
 function getSelected(style) {
-    if (selectedText) {
-        let div = document.createElement("span");
-        div.classList.add(style);
-        div.innerHTML = selectedText;
-        rangeOfText.deleteContents();
-        rangeOfText.insertNode(div);
-    }
-};
+  if (selectedText) {
+    const div = document.createElement("span");
+    div.classList.add(style);
+    div.innerHTML = selectedText;
+    range < OfText.deleteContents();
+    rangeOfText.insertNode(div);
+  }
+}
 
 function DeleteNote(e) {
-    let areYouSure = confirm("Vážně to chceš smazat?? A není to škoda??");
-    if (areYouSure) {
-      e.parentElement.parentElement.remove();
-      SaveNoteData();
-    }
-  };
+  const areYouSure = confirm("Vážně to chceš smazat?? A není to škoda??");
+  if (areYouSure) {
+    e.parentElement.parentElement.remove();
+    saveNoteData();
+  }
+}
