773fde9cfad42b8ef3a8da9678bd79709fbb322b
Žofie Tůmová <zofietumova@gmail.com>
2024-01-28 21:40:46 +0100

    sachovnice

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
-console.log("Test skriptu");
+const board = document.querySelector("#gameboard");
+
+const alphas = "abcdefgh".split("");
+const nums = "87654321".split("");
+
+alphas.forEach((col) => {
+  nums.forEach((row) => {
+    const divId = `${col}${row}`;
+    const div = document.createElement("div");
+    div.setAttribute("id", divId);
+    board.appendChild(div);
+  });
+});
+
