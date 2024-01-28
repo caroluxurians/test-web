d42948ec533940cd585bcdbaad06869c3e40b6c3
Žofie Tůmová <zofietumova@gmail.com>
2024-01-28 23:22:15 +0100

    piece init

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,9 @@ const board = document.querySelector("#gameboard");
 const alphas = "abcdefgh".split("");
 const nums = "87654321".split("");
 
-alphas.forEach((col, colIndex) => {
-  nums.forEach((row, rowIndex) => {
-    const divId = `${col}${row}`;
+nums.forEach((col, colIndex) => {
+  alphas.forEach((row, rowIndex) => {
+    const divId = `${row}${col}`;
     const div = document.createElement("div");
     if ((colIndex + rowIndex) % 2 === 1) {
       div.setAttribute("class", "black");
@@ -13,4 +13,19 @@ alphas.forEach((col, colIndex) => {
     div.setAttribute("id", divId);
     board.appendChild(div);
   });
+});
+
+const pieces = [
+  {
+    image: "public/pawnWhite.png",
+    position: "d2",
+  },
+];
+
+pieces.forEach((piece) => {
+  const positionDiv = document.getElementById(piece.position);
+  const image = document.createElement("img");
+  image.setAttribute("src", piece.image);
+  image.style.maxWidth = "90%"
+  positionDiv.appendChild(image);
 });
