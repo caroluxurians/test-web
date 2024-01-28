29021eff8ae9c010e48028ca091237fac95a120e
Žofie Tůmová <zofietumova@gmail.com>
2024-01-28 22:29:12 +0100

    chessboard rdy

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,14 @@ const board = document.querySelector("#gameboard");
 const alphas = "abcdefgh".split("");
 const nums = "87654321".split("");
 
-alphas.forEach((col) => {
-  nums.forEach((row) => {
+alphas.forEach((col, colIndex) => {
+  nums.forEach((row, rowIndex) => {
     const divId = `${col}${row}`;
     const div = document.createElement("div");
+    if ((colIndex + rowIndex) % 2 === 1) {
+      div.setAttribute("class", "black");
+    }
     div.setAttribute("id", divId);
     board.appendChild(div);
   });
 });
-
