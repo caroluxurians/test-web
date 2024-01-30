0c04b8a74eb736d9684c7faa01798c57102cedcc
Žofie Tůmová <zofietumova@gmail.com>
2024-01-30 21:06:08 +0100

    pawn news

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,37 @@ const board = document.querySelector("#gameboard");
 const alphas = "abcdefgh".split("");
 const nums = "87654321".split("");
 
+const pieces = [
+  {
+    id: "pw1",
+    image: "public/pawnWhite.png",
+    position: "d2",
+  },
+];
+
+let idOfPieceBeingMoved = null;
+
+const clearBoard = () => {
+  board.childNodes.forEach((el) => { el.innerHTML = ""; });
+};
+
+const renderPieces = () => {
+  pieces.forEach((piece) => {
+    const positionDiv = document.getElementById(piece.position);
+    const image = document.createElement("img");
+    image.setAttribute("src", piece.image);
+    image.style.maxWidth = "90%";
+    image.setAttribute("class", "piece");
+    image.setAttribute("id", piece.id);
+    image.setAttribute("draggable", "true");
+    image.addEventListener("dragstart", (ev) => {
+      idOfPieceBeingMoved = piece.id;
+      ev.dataTransfer.setData("image/png", ev.target.id);
+    });
+    positionDiv.appendChild(image);
+  });
+};
+
 nums.forEach((col, colIndex) => {
   alphas.forEach((row, rowIndex) => {
     const divId = `${row}${col}`;
@@ -10,30 +41,43 @@ nums.forEach((col, colIndex) => {
     if ((colIndex + rowIndex) % 2 === 1) {
       div.setAttribute("class", "black");
     }
+    div.addEventListener("dragenter", (e) => {
+      e.preventDefault();
+    });
+    div.addEventListener("dragover", (e) => {
+      e.preventDefault();
+    });
+    div.addEventListener("drop", (e) => {
+      const possibleNewPosition = e.target.id;
+      const selectedPiece = pieces.find((el) => el.id === idOfPieceBeingMoved);
+      // const pieceType = selectedPiece.id[0];
+      // const pieceColor = selectedPiece.id[1];
+      // totéž kratší (destrukturalizace):
+      const [pieceType, pieceColor] = selectedPiece.id;
+      const currentPosition = selectedPiece.position;
+      const [positionAlpha, positionNum] = currentPosition;
+      console.log(pieceType, pieceColor, positionAlpha, positionNum)
+      let isMoveLegal = false;
+      if (pieceType === "p") {
+
+      }
+      // if bílej pěšec
+      // current position: a2
+      // if possibleNewPosition: a3 or a4:
+      selectedPiece.position = possibleNewPosition;
+      // else nic
+      clearBoard();
+      renderPieces();
+    });
     div.setAttribute("id", divId);
     board.appendChild(div);
   });
 });
 
-const pieces = [
-  {
-    id: "pw1",
-    image: "public/pawnWhite.png",
-    position: "d2",
+renderPieces();
 
-  },
-];
-
-pieces.forEach((piece) => {
-  const positionDiv = document.getElementById(piece.position);
-  const image = document.createElement("img");
-  image.setAttribute("src", piece.image);
-  image.style.maxWidth = "90%";
-  image.setAttribute("class", "piece");
-  image.setAttribute("id", piece.id);
-  image.setAttribute("draggable", "true");
-  image.addEventListener("dragstart", (ev) => ev.dataTransfer.setData("text/plain", ev.target.id));
-  positionDiv.appendChild(image);
-  
-});
-
+//let dragStartPosition
+//div.dragStart(e) => {
+//dragStartPosition = e.target.parentNode.getAttribute("divId");
+//}
+//div.addEventListener("dragstart", dragStart);///
